907bb6c7d2c2e6936b39b6c7d46495f0d37dc8d3
MedhoMs <danielbucaloiu@gmail.com>
2026-03-18 13:24:48 +0000

    vue-i18n updated and legacy mode

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,7 @@
 import { createI18n } from 'vue-i18n'
 import { lang } from './lang.js'
 export const  i18n = createI18n({
+    legacy: false,
     messages: lang,
     fallbackLocale: 'es',
     locale: navigator.language || 'es',
