d83a6924a1c6eb0a14b63ecd31ca0b767b5378e1
MedhoMs <danielbucaloiu@gmail.com>
2026-03-18 13:47:48 +0000

    fix some outdated tailwind classes

--- src/components/GameModal.vue
+++ src/components/GameModal.vue
@@ -4,7 +4,7 @@
         <TransitionRoot as="template" :show="show">
         <Dialog as="div" class="relative z-10" @close="dismiss()">
             <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
-                <div class="fixed inset-0 bg-gray-700 bg-opacity-75 transition-opacity" />
+                <div class="fixed inset-0 bg-gray-700 bg/75 transition-opacity" />
             </TransitionChild>
 
             <div class="fixed z-10 inset-0 overflow-y-auto">
